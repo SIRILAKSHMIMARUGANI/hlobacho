5f7d94317b90706fee6dbdbfe2bc9d718ada3889
SIRILAKSHMIMARUGANI <sirilakshmimarugani@gmail.com>
2025-12-16 18:18:44 +0530

    msg

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
-const heading=React.createElement("h1",{id:"raba"},"Hello world!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+const Title=<h1>hello siri!</h1>
+const Heading1=()=>(
+    <h1>this is first heading1</h1>
+);
+const Heading2=()=>(
+    <div>
+        <h1>hello siri</h1>
+        <Heading1></Heading1>
+        <Heading1 />
+        {Heading1()}
+        {Title}
+    </div>
+)
 const root=ReactDOM.createRoot(document.getElementById("root"));
-const papaa=React.createElement("h2",{},"i am replaced by some element");
-const papa=ReactDOM.createRoot(document.getElementById("papa"));
-
-const parent=React.createElement("div",{id:"parent"},[(React.createElement("div",{id:"child1"},([React.createElement("h1",{id:"h1"},("this is h1")),(React.createElement("h2",{id:"h2"},("this is h2")))]),[React.createElement("div",{id:"child2"},[(React.createElement("h1",{id:"h1"},("this is h1"))),(React.createElement("h2",{id:"h2"},("this is h2")))])]))]);
-const fruit=ReactDOM.createRoot(document.getElementById("fruit"));
-fruit.render(parent);
+root.render(<Heading2/>)
